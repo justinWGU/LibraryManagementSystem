6cdd185b8bd831f3fe629551a23377890bf6027b
justinWGU <Justin_ortiz123@yahoo.com>
2024-07-26 19:34:18 -0700

    Adds Junit and testing directory

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class LibraryTest {
+
+    @Test
+    public void testSomething() {
+        assertEquals(2, 1+1, "Should equal 2");
+    }
+}
