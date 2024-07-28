a73ec9b8248a25dad5c52e0788f71089caba122f
justinWGU <Justin_ortiz123@yahoo.com>
2024-07-27 18:44:12 -0700

    Completes LibraryItem and Book classes

--- src/main/java/com/example/library/Book.java
+++ src/main/java/com/example/library/Book.java
@@ -1,4 +1,22 @@
 package com.example.library;
 
-public class Book {
+import java.util.Date;
+
+public class Book extends LibraryItem {
+
+    private int ISBN;
+    private int numPages;
+    private String author;
+
+    public Book(int ISBN, int numPages, String author, String title, Date publicationDate, String genre, int quantity) {
+        super(title, publicationDate,genre, quantity);
+        this.ISBN = ISBN;
+        this.numPages = numPages;
+        this.author = author;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Title: " + getTitle() + "Author: " + author + "Genre: " + getGenre());
+    }
 }
--- src/main/java/com/example/library/LibraryItem.java
+++ src/main/java/com/example/library/LibraryItem.java
@@ -1,4 +1,58 @@
 package com.example.library;
 
-public class LibraryItem {
+import java.util.Date;
+
+public abstract class LibraryItem {
+    // fields
+    private String title;
+    private Date publicationDate;
+    private String genre;
+    private int quantity;
+
+    // constructor
+    public LibraryItem(String title, Date publicationDate, String genre, int quantity) {
+        this.title = title;
+        this.publicationDate = publicationDate;
+        this.genre = genre;
+        this.quantity = quantity;
+    }
+
+    // abstract function
+    public abstract void displayInfo();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getDate() {
+        return publicationDate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
